1beb1eb4b59d61e1cf2f785594b41869fc642839
Viktor Bubnin <v.bubnin@molga.ru>
2021-08-25 18:21:11 +0300

    settings for deploy gh-pages

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { routesConfig } from '@routes/routesConfig';
 import { Header } from '@components/Header';
 import classes from "./App.module.css";
+import { REPO_NAME } from '@constants/repo';
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={`/${REPO_NAME}/`}>
       <div className={classes.wrapper}>
         <Header />
         <Switch>
